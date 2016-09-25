2d41c979ac6a6fda166651a26d0d40693df1a485
floodpeak <hongfengzhou@yahoo.com>
2016-09-25 16:44:15 +0800

    make list page data not hard code

--- src/main/java/com/odde/bbuddy/account/controller/AccountController.java
+++ src/main/java/com/odde/bbuddy/account/controller/AccountController.java
@@ -4,9 +4,13 @@ import com.odde.bbuddy.account.domain.Account;
 import com.odde.bbuddy.account.domain.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zbcjackson on 9/25/16.
@@ -33,7 +37,13 @@ public class AccountController {
     }
 
     @RequestMapping()
-    public String list(){
+    public String list(Model model){
+        List<Account> accounts = new ArrayList<>();
+        Account cashAccount = new Account();
+        cashAccount.setName("Cash");
+        cashAccount.setBalance(100);
+        accounts.add(cashAccount);
+        model.addAttribute("accounts", accounts);
         return "account/list";
     }
 }
